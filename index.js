dc064e68d2aa79c081638515168fa12b68f86db1
seriessoft <khaleelbasuny@yahoo.com>
2018-04-24 22:36:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var express = require('express');
-var app = express();
+var server = express();
 var port = process.env.PORT || 3000;
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-app.get('/',function(request, response){
+server.get('/',function(request, response){
   response.send("Hello!");
 });
 
-app.listen(port);
+server.listen(PORT);
 
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
