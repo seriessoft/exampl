f93cfeacbca81e1053233e992901f0133a20d1ad
seriessoft <khaleelbasuny@yahoo.com>
2018-04-24 22:07:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./lib/websocket');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/',function(request, response){
+  response.send("Hello!");
+});
+
+app.listen(port);
+
