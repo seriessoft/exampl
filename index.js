788f748dc51c3f90c2e97016f736818eac072c2f
seriessoft <khaleelbasuny@yahoo.com>
2018-04-24 22:53:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 5000;
 const SocketServer = require('ws').Server;
 const path = require('path');
 
