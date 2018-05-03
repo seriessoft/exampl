acefbcce0892c34344a5c39cd477847d3ec4203c
seriessoft <khaleelbasuny@yahoo.com>
2018-05-03 10:03:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ var app = express();
 var PORT = process.env.PORT || 5000;
 const SocketServer = require('ws').Server;
 const path = require('path');
-
+var Firebase = require('firebase');
 /*app.get('/',function(request, response){
   response.send("Hello!");
 });
 
 app.listen(PORT);*/
+
+var config = {
+    apiKey: "AIzaSyCj1gTHtLAbAYXAxbxyzMHWRCbKihvzdH8",
+    authDomain: "saudideal-922d4.firebaseapp.com",
+    databaseURL: "https://saudideal-922d4.firebaseio.com",
+    projectId: "saudideal-922d4",
+    storageBucket: "saudideal-922d4.appspot.com",
+    messagingSenderId: "820015895373"
+};
+
+Firebase.initializeApp(config);
 
 const INDEX = path.join(__dirname, 'index.html');
 
